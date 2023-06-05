10fd2e862a20dca13d9bec3e512b71ed782f3693
LuckyBitch <jobhunter@list.ru>
2023-06-05 13:49:49 +0400

    2n lesson props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+
+import Categories from './components/Categories';
+import Header from './components/Header';
+import PizzaBlock from './components/PizzaBlock';
+import Sort from './components/Sort';
+import './scss/app.scss'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello jumpeR!!!</h1>;
+
+<div class="wrapper">
+  <Header/>
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+<Categories/>
+<Sort/>
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+          <PizzaBlock title='привет!' price={500}/>
+          <PizzaBlock title='праопро!' price={600}/>
+          <PizzaBlock/>
+          <PizzaBlock/>
+          <PizzaBlock/>
+          <PizzaBlock/>
+          <PizzaBlock/>
+          <PizzaBlock/>
+          <PizzaBlock/>
+          <PizzaBlock/>
+          <PizzaBlock/>
+          <PizzaBlock/>
+          <PizzaBlock/>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
