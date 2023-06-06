a31c58654453b1c685497d9eb3572b98a3b5104d
LuckyBitch <jobhunter@list.ru>
2023-06-06 19:58:38 +0300

    3d lesson UseState

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
-
 import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
-import './scss/app.scss'
+import './scss/app.scss';
 
 function App() {
   return (
-
-<div class="wrapper">
-  <Header/>
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
-<Categories/>
-<Sort/>
+    <div className="wrapper">
+      <div className="block1">
+        <ul className="text2">
+          <li className="hello1">TEST INFO</li>
+          <li className="hello2">TEST INFO</li> <li className="hello3">TEST INFO</li>{' '}
+          <li className="hello4">TEST INFO</li>
+        </ul>
+      </div>
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-          <PizzaBlock title='привет!' price={500}/>
-          <PizzaBlock title='праопро!' price={600}/>
-          <PizzaBlock/>
-          <PizzaBlock/>
-          <PizzaBlock/>
-          <PizzaBlock/>
-          <PizzaBlock/>
-          <PizzaBlock/>
-          <PizzaBlock/>
-          <PizzaBlock/>
-          <PizzaBlock/>
-          <PizzaBlock/>
-          <PizzaBlock/>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="привет!" price={500} />
+            <PizzaBlock title="праопро!" price={600} />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
           </div>
         </div>
       </div>
     </div>
-
   );
 }
 
